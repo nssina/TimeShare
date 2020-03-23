d9e7b34082c5ce9fc0949c3120239c52d3c6b874
nssina <nssina@icloud.com>
2020-03-23 10:50:35 -0400

    Build 5
    Fix crashing issue and tableView configured

--- TimeShare MessagesExtension/EventViewController.swift
+++ TimeShare MessagesExtension/EventViewController.swift
@@ -27,4 +27,20 @@ class EventViewController: UIViewController {
     
 }
 
-
+extension EventViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Date", for: indexPath)
+        cell.textLabel?.text = "Date goes here"
+        
+        return cell
+    }
+}
