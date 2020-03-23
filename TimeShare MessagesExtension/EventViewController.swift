5c4484ea9dead3d16ed590a4840a7e5ec69987da
nssina <nssina@icloud.com>
2020-03-23 10:41:54 -0400

    Build 4
    Some changes in MessageViewController

--- TimeShare MessagesExtension/EventViewController.swift
+++ TimeShare MessagesExtension/EventViewController.swift
@@ -9,22 +9,22 @@
 import UIKit
 
 class EventViewController: UIViewController {
-
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
 
-        // Do any additional setup after loading the view.
+    @IBAction func saveSelectedDates(_ sender: UIButton) {
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addDate(_ sender: UIButton) {
+        
     }
-    */
-
+    
 }
+
+
--- TimeShare MessagesExtension/MessagesViewController.swift
+++ TimeShare MessagesExtension/MessagesViewController.swift
@@ -16,6 +16,36 @@ class MessagesViewController: MSMessagesAppViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func creatNewEvent(_ sender: UIButton) {
+        requestPresentationStyle(.expanded)
+    }
+    
+    func displayEventViewController(conversation: MSConversation?, identifier: String) {
+        // 0: sanity check, is there a conversation?
+        guard let conversation = conversation else { return }
+
+        // 1: create the child view controller
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: identifier) as? EventViewController else { return }
+
+        // 2: add the child to the parent so that events are forwarded
+        addChild(vc)
+
+        // 3: give the child a meaningful frame: make it fill our view
+        vc.view.frame = view.bounds
+        vc.view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(vc.view)
+
+        // 4: add Auto Layout constraints so the child view continues to fill the full view
+        vc.view.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        vc.view.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        vc.view.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        vc.view.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+
+        // 5: tell the child it has now moved to a new parent view controller
+        vc.didMove(toParent: self)
+    }
+    
+    
     // MARK: - Conversation Handling
     
     override func willBecomeActive(with conversation: MSConversation) {
@@ -53,9 +83,17 @@ class MessagesViewController: MSMessagesAppViewController {
     }
     
     override func willTransition(to presentationStyle: MSMessagesAppPresentationStyle) {
-        // Called before the extension transitions to a new presentation style.
-    
-        // Use this method to prepare for the change in presentation style.
+        
+        for child in children {
+            child.willMove(toParent: nil)
+            child.view.removeFromSuperview()
+            child.removeFromParent()
+        }
+        
+        if presentationStyle == .expanded {
+            displayEventViewController(conversation: activeConversation, identifier: "CreateEvent")
+        }
+        
     }
     
     override func didTransition(to presentationStyle: MSMessagesAppPresentationStyle) {
